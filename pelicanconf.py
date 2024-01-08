b77015d1dd4c1ce5ef4f764279365b8ed8390357
frankwu5099 <frankwu5099@gmail.com>
2024-01-07 19:02:20 -0500

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,53 @@
+AUTHOR = 'Po-Kuan Wu'
+SITENAME = 'PK WU 學習筆記'
+SITEURL = "."
+
+PATH = "content"
+
+TIMEZONE = 'America/Los_Angeles'
+ABOUT_ME = "It's so coooolu"
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+)
+
+# Social widget
+SOCIAL = (
+    ("Github", "https://github.com/frankwu5099"),
+    ("Linkedin", "https://www.linkedin.com/in/po-kuan-wu/"),
+)
+
+DEFAULT_PAGINATION = 10
+THEME = 'pelican-themes/pelican-bootstrap3'
+
+JINJA_ENVIRONMENT = {
+    "extensions": ["jinja2.ext.i18n"],
+}
+
+PLUGIN_PATHS = ['pelican-plugins']
+
+PLUGINS = ['i18n_subsites', 'render_math']
+
+I18N_TEMPLATES_LANG = 'en'
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+PYGMENTS_STYLE = 'vim'
+BOOTSTRAP_THEME  = 'yeti'
+CUSTOM_CSS = 'static/css/custom.css'
+STATIC_PATHS = [
+  'images',
+  'static'
+]
+#EXTRA_PATH_METADATA = {
+#    'extra/custom.css': {'path': 'static/css/custom.css'}
+#}
